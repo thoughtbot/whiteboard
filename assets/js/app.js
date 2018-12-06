4f181dd0fa2ffb9dabd475f036acc6507947216d
German Velasco <germsvel@gmail.com>
2018-12-06 15:32:20 -0500

    Set up basic elm app
    Sets up basic elm app that works with webpack and phoenix. For now it
    simply prints a Hello Elm and Phoenix.
    
    Also moves loading of app.js to show page.
    
    Co-authored-by: Chris Toomey <chris@ctoomey.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,9 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 import socket from "./socket"
+
+import { Elm } from "../src/Main.elm";
+
+var app = Elm.Main.init({
+    node: document.getElementById('elm-main')
+});
