933115b62372b443747ce3eed2cfdef3f94a6a72
Joël Quenneville <joelq@thoughtbot.com>
2018-12-06 17:02:00 -0500

    Copy over the Elm drawing app
    The way it's embedded on the page right now is a bit wonky. We may need
    to do some fancier CSS or let Elm own the whole page.

--- assets/src/Main.elm
+++ assets/src/Main.elm
@@ -1,7 +1,205 @@
-module Main exposing (main)
+port module Main exposing (main)
 
-import Html exposing (text)
+import Browser
+import Browser.Events
+import Html exposing (..)
+import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
+import Json.Decode as Decode exposing (Decoder)
+import Json.Encode as Encode
+import Svg exposing (Svg, polyline)
+import Svg.Attributes exposing (fill, points, stroke)
+import Time
 
 
+type alias Point =
+    ( Int, Int )
+
+
+type alias Path =
+    { id : Int
+    , points : List Point
+    }
+
+
+type Model
+    = Recording Path (List Path)
+    | Stable (List Path)
+
+
+toList : Model -> List Path
+toList model =
+    case model of
+        Recording head tail ->
+            head :: tail
+
+        Stable list ->
+            list
+
+
+type alias Flags =
+    ()
+
+
+initialPaths : List Path
+initialPaths =
+    []
+
+
+initialize : Flags -> ( Model, Cmd Msg )
+initialize flags =
+    ( Stable initialPaths, Cmd.none )
+
+
+svgPoint : Point -> String
+svgPoint ( x, y ) =
+    String.fromInt x ++ "," ++ String.fromInt y
+
+
+svgPathString : Path -> String
+svgPathString path =
+    String.join " " (List.map svgPoint path.points)
+
+
+startRecording : Model -> Model
+startRecording model =
+    case model of
+        Recording _ _ ->
+            model
+
+        Stable paths ->
+            Recording { id = nextId paths, points = [] } paths
+
+
+nextId : List Path -> Int
+nextId paths =
+    paths
+        |> List.map .id
+        |> List.maximum
+        |> Maybe.withDefault 0
+        |> (+) 1
+
+
+stopRecording : Model -> Model
+stopRecording model =
+    case model of
+        Recording _ _ ->
+            Stable (toList model)
+
+        Stable _ ->
+            model
+
+
+addPoint : Point -> Model -> Model
+addPoint point model =
+    case model of
+        Recording current rest ->
+            Recording { current | points = point :: current.points } rest
+
+        Stable _ ->
+            model
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        DrawStarted ->
+            ( startRecording model, Cmd.none )
+
+        DrawEnded ->
+            ( stopRecording model, sendBuildingPoints model )
+
+        PointAdded point ->
+            ( addPoint point model, Cmd.none )
+
+        NetworkClockTick ->
+            ( model, sendBuildingPoints model )
+
+
+sendBuildingPoints : Model -> Cmd a
+sendBuildingPoints model =
+    buildingPoints model
+        |> Maybe.map (sendPoints << encodePath)
+        |> Maybe.withDefault Cmd.none
+
+
+buildingPoints : Model -> Maybe Path
+buildingPoints model =
+    case model of
+        Recording current _ ->
+            Just current
+
+        Stable _ ->
+            Nothing
+
+
+viewPath : Path -> Svg a
+viewPath path =
+    polyline [ points (svgPathString path), fill "none", stroke "black" ] []
+
+
+view : Model -> Html Msg
+view model =
+    Svg.svg
+        [ style "position" "absolute"
+        , style "width" "100%"
+        , style "height" "100%"
+        ]
+        (List.map viewPath <| toList model)
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    case model of
+        Recording _ _ ->
+            Sub.batch
+                [ Browser.Events.onMouseUp (Decode.succeed DrawEnded)
+                , Sub.map PointAdded (Browser.Events.onMouseMove mousePositionDecoder)
+                , Time.every 50 (\_ -> NetworkClockTick)
+                ]
+
+        Stable _ ->
+            Browser.Events.onMouseDown (Decode.succeed DrawStarted)
+
+
+mousePositionDecoder : Decoder Point
+mousePositionDecoder =
+    Decode.map2 Tuple.pair
+        (Decode.field "clientX" Decode.int)
+        (Decode.field "clientY" Decode.int)
+
+
+type Msg
+    = DrawStarted
+    | DrawEnded
+    | PointAdded Point
+    | NetworkClockTick
+
+
+port sendPoints : Decode.Value -> Cmd a
+
+
+encodePath : Path -> Decode.Value
+encodePath path =
+    Encode.object
+        [ ( "id", Encode.int path.id )
+        , ( "points", Encode.list encodePoint path.points )
+        ]
+
+
+encodePoint : Point -> Decode.Value
+encodePoint ( x, y ) =
+    Encode.object
+        [ ( "x", Encode.int x )
+        , ( "y", Encode.int y )
+        ]
+
+
+main : Program Flags Model Msg
 main =
-    text "Hello Elm and Phoenix!"
+    Browser.element
+        { init = initialize
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
